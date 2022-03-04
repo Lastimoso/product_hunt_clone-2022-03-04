6ec88b7a02702b1ea6c05f45b419cb045e6c304a
Gerardo Raiden <gerardoraiden@gmail.com>
2022-03-04 11:40:26 -0300

    System test done

--- test/system/products_test.rb
+++ test/system/products_test.rb
@@ -13,4 +13,28 @@ class ProductsTest < ApplicationSystemTestCase
     assert_selector "h1", text: "Awesome Products"
     assert_selector ".card-product", count: Product.count
   end
+
+  test "creating a product" do
+    # setup
+    login_as users(:homer)
+
+    # exericise
+    visit "products/new"
+
+    fill_in "product_tagline", with: "La mejor de la ciudad"
+
+    click_on "Create Product"
+
+    assert_text "Name can't be blank"
+
+    fill_in "product_name",  with: "Empanada loca"
+
+    click_on "Create Product"
+    # assert / verify
+
+    assert_equal root_path, page.current_path
+    assert_text "La mejor de la ciudad"
+
+    # teardown
+  end
 end
