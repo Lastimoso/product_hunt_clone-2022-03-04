66f701452b563855e43a49e1c8648de6541293ed
Gerardo Raiden <gerardoraiden@gmail.com>
2022-03-04 11:09:41 -0300

    FFiAdd test

---
+++ test/system/products_test.rb
@@ -0,0 +1,16 @@
+require "application_system_test_case"
+
+class ProductsTest < ApplicationSystemTestCase
+  # test "visiting the index" do
+  #   visit products_url
+  #
+  #   assert_selector "h1", text: "Product"
+  # end
+
+  test "visiting the home" do
+    visit root_url
+
+    assert_selector "h1", text: "Awesome Products"
+    assert_selector ".card-product", count: Product.count
+  end
+end
